c7d4c66512449e87645beb6e6ac48e6d79100c7f
eef22 <eford1@binghamton.edu>
2017-01-30 14:53:54 -0500

    done! (I think)

--- script.js
+++ script.js
@@ -27,10 +27,54 @@ $(document).ready(function() {
 
 		}
 
-		if($(".star").length > 237) {
-			location.reload();
-		};
+if(location.search !=="?forReal"){
 
+		if($(".star").length > 10) {
+
+			$("body").empty().css("background-color", "#ffb3b3").append("<div class='snark'>Don't you have anything better to do?</div>")
+
+			setTimeout(function(){
+				$("body").empty().append("<div class='snark'>No?</div>")
+			},3000);
+
+			setTimeout(function(){
+				$("body").empty().append("<div class='snark' style='font-size: 75px'>Well then, carry on.<br>Loading more bubble wrap in</div>")
+			},6000);
+
+			setTimeout(function(){
+				$("body").append("<div class='snark'>...3...</div>")
+			},7000);
+
+			setTimeout(function(){
+				$("body").append("<div class='snark'>...2...</div>")
+			},8000);
+
+			setTimeout(function(){
+				$("body").append("<div class='snark'>...1...</div>")
+			},9000);
+
+			setTimeout(function(){
+				location.search = "?forReal";
+			},10000);
+		};
+}
+
+	if($(".star").length > 237) {
+		$("body").empty().css("background-color", "#85e085").append("<div class='congrats'><b>Congratulations!<b></div>")
+
+		setTimeout(function(){
+			$("body").append("<div class='congrats'><br>You're either really dedicated...</div>")
+		},2000);
+
+		setTimeout(function(){
+			$("body").append("<div class='congrats'>...or really bored.</div>")
+		},4000);
+
+		setTimeout(function(){
+			location.reload();
+		},7000);
+
+	}
 	});
 
 });
